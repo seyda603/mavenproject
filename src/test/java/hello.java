5abf56aa949435288f6ad77aad95084b800da96e
seyda603 <selcanseydaaydin@gmail.com>
2021-09-14 11:14:34 +0300

    initial commit started hello feathur added more hello fetture

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -3,5 +3,7 @@ public class hello {
 
         System.out.println("hello");
         System.out.println("Develophing hello feature");
+        System.out.println("github repo is alredy publideh to github");
+        System.out.println("doing more work");
     }
 }
